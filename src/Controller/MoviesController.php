55e24765c287a47e3fe0f58e730eb058d796034b
Laura <laura.hantz@hotmail.fr>
2020-01-06 15:16:06 +0100

    Mise à jour,

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Movies;
+
+class MoviesController extends AbstractController
+{
+    /**
+     * @Route("/movies", name="movies")
+     */
+    public function index()
+    {
+
+        $movies = $this->getDoctrine()
+        ->getRepository(Movies::class)
+        ->findBy([], ['name' =>'ASC'], 20, 0);
+        return $this->render('movies/index.html.twig', [
+            'controller_name' => 'MoviesController',
+            'movies' => $movies
+        ]);
+    }
+}
