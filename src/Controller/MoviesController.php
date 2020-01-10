f521f7eb315e12a46f88810736c2f3b5d3991e88
Laura <laura.hantz@hotmail.fr>
2020-01-10 11:18:57 +0100

    Gestion erreur

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Movies;
@@ -11,6 +12,8 @@ class MoviesController extends AbstractController
 {
     /**
      * @Route("/movies", name="movies")
+     * @param Request $request
+     * @return Response
      */
     public function index(Request $request)
     {
@@ -35,13 +38,20 @@ class MoviesController extends AbstractController
 
     /**
      * @Route("/movie/{id}", name="movie")
+     * @param $id
+     * @return Response
      */
     public function movie($id)
     {
       $movie = $this->getDoctrine()
       ->getRepository(Movies::class)
       ->find($id);
-      dump($movie);
+
+      if (!$movie) {
+        return $this->redirectToRoute('movies');
+      }
+
+      
       return $this->render('movies/movie.html.twig', [
           'movie' => $movie
       ]);
